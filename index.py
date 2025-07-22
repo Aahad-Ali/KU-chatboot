b0b4232f23ae6805b15726f457638546946049c6
Aahad Ali <104500845+Aahad-Ali@users.noreply.github.com>
2025-07-22 12:10:07 -0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,26 @@
+import os
+
+
+print(os.listdir())
+
+
+print("hello world")
+# print("This is a simple Python script.")
+# print("It prints messages to the console.")
+# print("Goodbye!")
+# print("End of script.")
+# print("Thank you for running this script.")
+# print("Have a great day!")
+
+
+a = 5
+b = 4
+
+
+# print("The sum of", a, "and", b, "is", a + b)
+# print("The sub of", a, "and", b, "is", a - b)
+# print("The multiple of", a, "and", b, "is", a * b)
+# print("The division of", a, "and", b, "is", a / b)
+
+# a -= 3
+# print("The new value of a is", a)
